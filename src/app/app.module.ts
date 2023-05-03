ae82f55346680d51d8116a2ce2fd271aaa147930
oussemajaberi <jabberioussama@gmail.com>
2023-05-03 15:34:04 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { NavbarComponent } from './includes/navbar/navbar.component';
+export function Kcfactory(kcService: KeycloakService) {
+  return () =>
+   kcService.init({
 
+      config: {
+        url: 'http://localhost:8180/auth/',
+        realm: 'Auth-user',
+        clientId:'client-ang',
+
+
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        checkLoginIframe:true
+
+      },
+    });
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {provide : APP_INITIALIZER, deps : [KeycloakService],useFactory : Kcfactory, multi : true},
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
